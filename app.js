05ed150f7f716b9348a8c329198fad01560e78fc
Brandon Tapia <btapia@ucol.mx>
2022-03-18 15:24:52 -0600

    comentarios

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from 'express'; //Importamos la libreria.
 const app = new express();
 
 app.get('/', (req, res) => { //route handler
-    res.send('Hello World, this is the root route');
+    res.send('Hello World, this is the root route'); //envio de información
 });
 
-app.listen(3000);
+app.listen(3000); //Permite escuchar al servidor.
 app.get('/uno', (req, res) => { //route handler
-    res.send('Hello World, from the route One');
+    res.send('Hello World, from the route One'); //envio de información
 });
 
 app.get('/prueba', (req, res) => { //route handler
-    res.send('Hello World, from route Prueba');
+    res.send('Hello World, from route Prueba'); //envio de información
 });
 
